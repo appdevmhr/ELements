340c1c24ced18d9cc867cee87536c5c82fc27a4e
appdevmhr <appdevmhr@gmail.com>
2023-07-04 13:16:30 +0600

    Initial commit

---
+++ Elements/src/main/java/appdevmhr/honeyappdeveloper/elements/Elements.kt
@@ -0,0 +1,9 @@
+package appdevmhr.honeyappdeveloper.elements
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Elements() {
+    Text(text = "Hello from Elements!")
+}
